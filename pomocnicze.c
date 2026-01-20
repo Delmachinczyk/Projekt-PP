e8a69f7ab6d539f9c71d0c93825ef22063470778
Delmachinczyk <pbieniasz0525@gmail.com>
2026-01-20 21:49:28 +0100

    dodanie funkcji pomocniczych takich jak wczytanie tekstu,zagrożenia wraz z walidacją danych czyszczenie bufora

---
+++ pomocnicze.c
@@ -0,0 +1,32 @@
+#include "pomocnicze.h"
+#include <string.h>
+#include <stdlib.h>
+
+void czyszczenieBufora(void){
+    int dane;
+    while((dane = getchar()) != '\n' && dane != EOF);
+}
+
+void wczytajTekst(char* tekst, int max_dlugosc){
+    if(fgets(tekst, max_dlugosc, stdin) != NULL){
+        size_t dlugosc_tekstu = strlen(tekst);
+        if (dlugosc_tekstu > 0 && tekst[dlugosc_tekstu - 1] == '\n') {
+            tekst[dlugosc_tekstu - 1] = '\0';
+        }else{
+            czyszczenieBufora();
+        }
+    }
+}
+
+int wczytajLiczbe(void){
+    int x;
+    while(scanf("%d", &x) != 1){
+        printf("Podaj liczbe calkowaita: ");
+        czyszczenieBufora();
+    }
+    czyszczenieBufora();
+    return x;
+}
+int walidacjaZagrozenia(int zagrozenie){
+    return (zagrozenie >= 0 && zagrozenie <= 10);
+}
---
+++ pomocnicze.h
@@ -0,0 +1,7 @@
+#ifndef POMOCNICZE_H
+#define POMOCNICZE_H
+void czyszczenieBufora(void);
+void wczytajTekst(char* tekst, int max_dlugosc);
+int wczytajLiczbe(void);
+int walidacjaZagrozenia(int zagrozenie);
+#endif
