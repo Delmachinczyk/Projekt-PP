28116e11b9a02ff267a906017a63ff8512932f6b
Delmachinczyk <pbieniasz0525@gmail.com>
2026-01-20 12:41:08 +0100

    Dodanie deklaracji struktury Node oraz funkcji zarządzających listą dynamiczną

---
+++ lista.h
@@ -0,0 +1,29 @@
+#ifndef LISTA_H
+#define LISTA_H
+
+#include "postac.h"
+
+typedef struct Node{
+    Postac dane;
+    struct Node* next;
+}Node;
+
+Node* utworzListe();
+
+void dodajPostac(Node** head, Postac nowaPostac);
+
+void usunPostac(Node** head, const char* pseudonim);
+
+void wyswietl(Node *head);
+
+void wyszukajPoPseudonimie(Node* head, const char* pseudonim);
+
+void wyszukajPoZagrozeniu(Node* head, int zagrozenie);
+
+Node* sortujAlfabetycznie(Node* head);
+
+Node* sortujPoZagrozeniu(Node* head);
+
+void zwolnijPamiec(Node* head);
+
+#endif
